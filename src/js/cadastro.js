a29e741951c39bf483ce2f17d81541b145f01823
Guilherme Augusto <guilhermealc01@gmail.com>
2023-12-05 00:06:55 -0300

    corrigindo erro cadastro

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 //visualizar senha
 let btn = document.querySelector('#verSenha')
 let btnConfirm = document.querySelector('#verConfirmSenha')
@@ -12,6 +13,14 @@ let btnConfirm = document.querySelector('#verConfirmSenha')
 let email = document.querySelector('#nome')
 let labelEmail = document.querySelector('#labelNome')
 >>>>>>> 7c9ba0f (Enviando JavaScript do Cadastro)
+=======
+//visualizar senha
+let btn = document.querySelector('#verSenha')
+let btnConfirm = document.querySelector('#verConfirmSenha')
+
+let email = document.querySelector('#email')
+let labelEmail = document.querySelector('#labelEmail')
+>>>>>>> d0782cb (corrigindo erro cadastro)
 let validEmail = false
 
 let nome = document.querySelector('#nome')
@@ -62,6 +71,7 @@ nome.addEventListener('keyup', () => {
       validNome = true
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
 })
 
 usuario.addEventListener('keyup', () => {
@@ -109,50 +119,57 @@ confirmSenha.addEventListener('keyup', () => {
 //visualizar senha
 =======
   })
+=======
+})
+>>>>>>> d0782cb (corrigindo erro cadastro)
 
-  usuario.addEventListener('keyup', () => {
-    if(usuario.value.length <= 4){
-      labelUsuario.setAttribute('style', 'color: red')
-      labelUsuario.innerHTML = 'Usuário [Insira no minimo 5 caracteres]'
-      usuario.setAttribute('style', 'border-color: red')
-      validUsuario = false
-    } else {
-      labelUsuario.setAttribute('style', 'color: green')
-      labelUsuario.innerHTML = 'Usuário'
-      usuario.setAttribute('style', 'border-color: green')
-      validUsuario = true
-    }
-  })
+usuario.addEventListener('keyup', () => {
+  if(usuario.value.length <= 4){
+    labelUsuario.setAttribute('style', 'color: red')
+    labelUsuario.innerHTML = 'Usuário [Insira no minimo 5 caracteres]'
+    usuario.setAttribute('style', 'border-color: red')
+    validUsuario = false
+  } else {
+    labelUsuario.setAttribute('style', 'color: green')
+    labelUsuario.innerHTML = 'Usuário'
+    usuario.setAttribute('style', 'border-color: green')
+    validUsuario = true
+  }
+})
 
-  senha.addEventListener('keyup', () => {
-    if(senha.value.length <= 5){
-      labelSenha.setAttribute('style', 'color: red')
-      labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
-      senha.setAttribute('style', 'border-color: red')
-      validSenha = false
-    } else {
-      labelSenha.setAttribute('style', 'color: green')
-      labelSenha.innerHTML = 'Senha'
-      senha.setAttribute('style', 'border-color: green')
-      validSenha = true
-    }
-  })
+senha.addEventListener('keyup', () => {
+  if(senha.value.length <= 5){
+    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
+    senha.setAttribute('style', 'border-color: red')
+    validSenha = false
+  } else {
+    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.innerHTML = 'Senha'
+    senha.setAttribute('style', 'border-color: green')
+    validSenha = true
+  }
+})
 
-  confirmsenha.addEventListener('keyup', () => {
-    if(senha.value != confirmsenha.value){
-      labelConfirmSenha.setAttribute('style', 'color: red')
-      labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
-      s.setAttribute('style', 'border-color: red')
-      validConfirmSenha = false
-    } else {
-      labelConfirmSenha.setAttribute('style', 'color: green')
-      labelConfirmSenha.innerHTML = 'Confirmar Senha'
-      s.setAttribute('style', 'border-color: green')
-      validConfirmSenha = true
-    }
-  })
+confirmSenha.addEventListener('keyup', () => {
+  if(senha.value != confirmSenha.value){
+    labelConfirmSenha.setAttribute('style', 'color: red')
+    labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
+    confirmSenha.setAttribute('style', 'border-color: red')
+    validConfirmSenha = false
+  } else {
+    labelConfirmSenha.setAttribute('style', 'color: green')
+    labelConfirmSenha.innerHTML = 'Confirmar Senha'
+    confirmSenha.setAttribute('style', 'border-color: green')
+    validConfirmSenha = true
+  }
+})
   
+<<<<<<< HEAD
 >>>>>>> 7c9ba0f (Enviando JavaScript do Cadastro)
+=======
+//visualizar senha
+>>>>>>> d0782cb (corrigindo erro cadastro)
 btn.addEventListener('click', () => {
     let inputsenha = document.querySelector('#senha')
 
@@ -166,10 +183,14 @@ btn.addEventListener('click', () => {
 
 btnConfirm.addEventListener('click', () => {
 <<<<<<< HEAD
+<<<<<<< HEAD
     let inputConfirmSenha = document.querySelector('#confirmSenha')
 =======
     let inputConfirmSenha = document.querySelector('#confirmsenha')
 >>>>>>> 7c9ba0f (Enviando JavaScript do Cadastro)
+=======
+    let inputConfirmSenha = document.querySelector('#confirmSenha')
+>>>>>>> d0782cb (corrigindo erro cadastro)
 
     if (inputConfirmSenha.getAttribute('type') == 'password') {
         inputConfirmSenha.setAttribute('type', 'text')
@@ -177,6 +198,7 @@ btnConfirm.addEventListener('click', () => {
         inputConfirmSenha.setAttribute('type', 'password')
     }
 })
+<<<<<<< HEAD
 <<<<<<< HEAD
 //fim visualizar senha
 
@@ -212,32 +234,15 @@ function cadastrar(){
   }
 }
 =======
+=======
+//fim visualizar senha
+>>>>>>> d0782cb (corrigindo erro cadastro)
 
 function cadastrar(){
-    if(validEmail && validNome && validUsuario && validSenha && validConfirmSenha){
-      let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
-      
-      listaUser.push(
-      {
-        emailCad: email.value,
-        nomeCad: nome.value,
-        userCad: usuario.value,
-        senhaCad: senha.value
-      }
-      )
-      
-      localStorage.setItem('listaUser', JSON.stringify(listaUser))
-      
-     
-      msgSuccess.setAttribute('style', 'display: block')
-      msgSuccess.innerHTML = '<strong>Cadastrado com sucesso!</strong>'
-      msgError.setAttribute('style', 'display: none')
-      msgError.innerHTML = ''
-      
-      setTimeout(()=>{
-          window.location.href = 'http://127.0.0.1:5500/src/pages/login.html'
-      }, 3000)
+  if(validEmail && validNome && validUsuario && validSenha && validConfirmSenha){
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
     
+<<<<<<< HEAD
       
     } else {
       msgError.setAttribute('style', 'display: block')
@@ -247,3 +252,32 @@ function cadastrar(){
     }
   }
 >>>>>>> 7c9ba0f (Enviando JavaScript do Cadastro)
+=======
+    listaUser.push(
+    {
+      emailCad: email.value,
+      nomeCad: nome.value,
+      userCad: usuario.value,
+      senhaCad: senha.value
+    })
+    
+    localStorage.setItem('listaUser', JSON.stringify(listaUser))
+    
+    
+    msgSuccess.setAttribute('style', 'display: block')
+    msgSuccess.innerHTML = '<strong>Cadastrado com sucesso!</strong>'
+    msgError.setAttribute('style', 'display: none')
+    msgError.innerHTML = ''
+    
+    setTimeout(() => {
+      window.location.href = '/src/pages/login.html';
+    })
+    
+  } else {
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
+    msgSuccess.innerHTML = ''
+    msgSuccess.setAttribute('style', 'display: none')
+  }
+}
+>>>>>>> d0782cb (corrigindo erro cadastro)
