0105be002112207d6ba7cb900993cfb87fbe7e1f
guilherme-alc <guilhermealc01@gmail.com>
2023-12-10 13:21:11 -0300

    atualizando funcionalidade da galeria

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -1,97 +1,79 @@
-const inputFile = document.querySelector("#picture__input");
-const pictureImage = document.querySelector(".picture__image");
-const pictureImageTxt = "Choose an image";
-pictureImage.innerHTML = pictureImageTxt;
+document.addEventListener("DOMContentLoaded", function () {
+    const inputFile = document.getElementById("picture__input");
+    const photoFrames = document.querySelectorAll('.photo-frame');
+    let currentFrameIndex = 0;
 
-inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target;
-  const file = inputTarget.files[0];
-
-  if (file) {
-    const reader = new FileReader();
-
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target;
-
-      const img = document.createElement("img");
-      img.src = readerTarget.result;
-      img.classList.add("picture__img");
-
-      pictureImage.innerHTML = "";
-      pictureImage.appendChild(img);
+    // Adiciona um evento de mudança para o input de arquivo
+    inputFile.addEventListener('change', function (event) {
+        addImageToFrame(event, currentFrameIndex);
+        currentFrameIndex = (currentFrameIndex + 1) % photoFrames.length;
     });
 
-    reader.readAsDataURL(file);
-  } else {
-    pictureImage.innerHTML = pictureImageTxt;
-  }
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const photoFrames = document.querySelectorAll('.photo-frame');
-
-    // Adicionar evento de clique a cada photo-frame
-    photoFrames.forEach(photoFrame => {
+    // Adiciona evento de clique a cada photo-frame
+    photoFrames.forEach((photoFrame, index) => {
         photoFrame.addEventListener('click', function () {
-            // Captura o elemento da imagem dentro do photo-frame clicado
-            const image = this.querySelector('img');
-            // Cria um elemento de overlay para a imagem em tela cheia
-            const overlay = document.createElement('div');
-            overlay.classList.add('overlay');
-            // Cria a imagem em tela cheia
-            const fullScreenImage = new Image();
-            fullScreenImage.src = image.src;
-            fullScreenImage.classList.add('full-screen-image');
-
-            // Adiciona a imagem em tela cheia ao overlay
-            overlay.appendChild(fullScreenImage);
-
-            // Adiciona o overlay ao corpo do documento
-            document.body.appendChild(overlay);
-
-            // Adiciona evento de clique para fechar a tela cheia
-            overlay.addEventListener('click', function () {
-                document.body.removeChild(overlay);
-            });
+            // Define o índice atual ao clicar em uma photo-frame
+            currentFrameIndex = index;
+            inputFile.click();
         });
     });
+
+    // Carrega imagens do localStorage ao carregar a página
+    loadImagesFromLocalStorage();
+
+    // Define o texto inicial para a tag pictureImage (se existir)
+    const pictureImage = document.querySelector('.picture__image');
+    if (pictureImage) {
+        const pictureImageTxt = "Choose an image";
+        pictureImage.innerHTML = pictureImageTxt;
+    }
 });
-// Função para adicionar uma imagem ao photo frame clicado
-function addImageToFrame(event) {
+
+// Função para adicionar uma imagem a uma photo-frame específica
+function addImageToFrame(event, frameIndex) {
     const file = event.target.files[0];
+
     if (file) {
         const reader = new FileReader();
 
         reader.onload = function (event) {
             const imageUrl = event.target.result;
+
+            // Salva a imagem no localStorage
+            saveImageToLocalStorage(frameIndex, imageUrl);
+
+            // Seleciona a photo-frame pelo índice e substitui a imagem
+            const targetFrame = document.querySelectorAll('.photo-frame')[frameIndex];
             const img = document.createElement('img');
             img.src = imageUrl;
 
-            // Substitui a imagem no photo frame clicado
-            event.currentTarget.querySelector('img').replaceWith(img);
+            // Substitui a imagem na photo frame específica
+            targetFrame.innerHTML = '';
+            targetFrame.appendChild(img);
         };
 
         reader.readAsDataURL(file);
     }
 }
 
-// Event listener para o input de arquivo
-const fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', function (event) {
-    addImageToFrame(event);
-    // Limpa o valor do input para permitir adicionar a mesma imagem novamente
-    this.value = '';
-});
+// Função para salvar a imagem no localStorage
+function saveImageToLocalStorage(index, imageUrl) {
+    const images = JSON.parse(localStorage.getItem('images')) || [];
+    images[index] = imageUrl;
+    localStorage.setItem('images', JSON.stringify(images));
+}
 
-// Event listener para adicionar imagens aos photo frames clicados
-const photoFrames = document.getElementById('photoFrames');
-photoFrames.addEventListener('click', function (event) {
-    const target = event.target;
-    if (target.tagName === 'IMG') {
-        fileInput.click();
-    }
-});
+// Função para carregar as imagens do localStorage ao carregar a página
+function loadImagesFromLocalStorage() {
+    const images = JSON.parse(localStorage.getItem('images')) || [];
 
+    images.forEach((imageUrl, index) => {
+        const targetFrame = document.querySelectorAll('.photo-frame')[index];
+        const img = document.createElement('img');
+        img.src = imageUrl;
 
+        // Substitui a imagem na photo frame específica
+        targetFrame.innerHTML = '';
+        targetFrame.appendChild(img);
+    });
+}
